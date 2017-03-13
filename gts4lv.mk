fa24d5af67a66f6a5770060a8d73f8a890fa7465
Badhri Jagan Sridharan <Badhri@google.com>
2017-03-13 15:12:36 -0700

    gts4lv-common: USB HAL: initial type-c hal
    Bug: 36097487
    Test: Manually tested role swap and UI behavior
    Change-Id: I5ce520da521e0714001b60164ac850dddbd644c3

--- gts4lv.mk
+++ gts4lv.mk
@@ -275,6 +275,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm710
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.device
+
 # Wi-Fi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/indoorchannel.info:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/indoorchannel.info \
