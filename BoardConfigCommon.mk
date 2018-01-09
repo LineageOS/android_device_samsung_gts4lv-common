db034cc0c66ffa28e1d11fa73325dbbddcd2ed7f
Suren Baghdasaryan <surenb@google.com>
2018-01-09 15:47:40 -0800

    gts4lv-common: Set lmkd configuration properties
    Bug: 63631020
    
    Change-Id: I16a183e8e4625e8b98cefdc9c8a7b650556a2b55
    Signed-off-by: Suren Baghdasaryan <surenb@google.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -185,6 +185,7 @@ TARGET_USES_MKE2FS := true
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
+TARGET_PRODUCT_PROP += $(COMMON_PATH)/product.prop
 TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
