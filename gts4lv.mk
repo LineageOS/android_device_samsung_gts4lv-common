ba7f7137a0ea736499b7762b7a8cf689d601b718
Jeff Tinker <jtinker@google.com>
2018-11-05 15:01:48 -0800

    gts4lv-common: Enable clearkey drm plugin v1.2
    test: android.media.cts.MediaDrmClearkeyTest#testOfflineKeyManagement
    
    bug: 117570956
    bug: 110838441
    bug: 116252891
    Change-Id: I86fedc9d383f90cf874c8001f7064b9bfbbe3658

--- gts4lv.mk
+++ gts4lv.mk
@@ -132,7 +132,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.1-service.clearkey
+    android.hardware.drm@1.2-service.clearkey
 
 # Fingerprint
 PRODUCT_PACKAGES += \
