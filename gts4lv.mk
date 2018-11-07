60d0038f1cd193cc1f66447698924b5426344bd0
Anton Hansson <hansson@google.com>
2018-11-07 16:10:05 +0000

    gts4lv-common: Enforce RRO for all resource overlays
    Bug: 118823052
    Test: make
    Change-Id: I579d09480c7a482543ea616dd5c493d825bb98b1

--- gts4lv.mk
+++ gts4lv.mk
@@ -38,8 +38,7 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
-PRODUCT_ENFORCE_RRO_TARGETS += \
-    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS += *
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
