0eb4485c75e072dcadbaacb57c50ba297eb9e7d1
LuK1337 <priv.luk@gmail.com>
2019-08-14 21:44:36 +0200

    gts4lv-common: Somewhat clean up vendor_prop.mk
    Change-Id: Icd46365c618bc1a9f8a690e1bdc32b6a8f950c9e

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -1,113 +1,123 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heapsize=512m \
-    dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapmaxfree=8m \
-    ro.hardware.nfc_nci=nqx.default \
-    persist.vendor.qcomsysd.enabled=1 \
-    ro.security.vaultkeeper.native=1 \
-    keyguard.no_require_sim=true \
-    ro.carrier=unknown \
-    persist.vendor.radio.atfwd.start=false \
-    ro.smps.enable=true \
-    ro.error.receiver.default=com.samsung.receiver.error \
-    security.ASKS.policy_version=000000 \
-    ro.security.fips_bssl.ver=1.3 \
-    ro.security.fips_skc.ver=1.8 \
-    ro.security.fips_scrypto.ver=2.0 \
-    ro.vendor.extension_library=libqti-perfd-client.so \
-    persist.vendor.radio.apm_sim_not_pwdn=1 \
-    persist.vendor.radio.sib16_support=1 \
-    persist.vendor.radio.custom_ecc=1 \
-    persist.vendor.radio.rat_on=combine \
-    persist.backup.ntpServer=0.pool.ntp.org \
-    sys.vendor.shutdown.waittime=500 \
-    persist.demo.hdmirotationlock=false \
-    ro.vendor.display.cabl=2 \
-    debug.sf.hw=0 \
-    debug.egl.hw=0 \
-    debug.sf.latch_unsignaled=1 \
-    vendor.display.enable_default_color_mode=1 \
     af.fast_track_multiplier=1 \
-    vendor.audio_hal.period_size=192 \
-    ro.vendor.audio.sdk.fluencetype=none \
-    persist.vendor.audio.fluence.voicecall=true \
-    persist.vendor.audio.fluence.voicerec=false \
-    persist.vendor.audio.fluence.speaker=true \
-    persist.vendor.audio.fluence.audiorec=false \
-    persist.vendor.audio.fluence.tmic.enabled=false \
-    vendor.audio.apptype.multirec.enabled=false \
-    vendor.audio.record.multiple.enabled=false \
-    persist.vendor.audio.spv3.enable=true \
-    persist.vendor.audio.avs.afe_api_version=2 \
-    vendor.audio.tunnel.encode=false \
-    persist.vendor.audio.ras.enabled=false \
-    vendor.audio.offload.buffer.size.kb=64 \
+    audio.deep_buffer.media=true \
     audio.offload.min.duration.secs=30 \
     audio.offload.video=true \
-    vendor.audio.offload.track.enable=true \
-    audio.deep_buffer.media=true \
-    vendor.voice.path.for.pcm.voip=true \
-    vendor.audio.offload.multiaac.enable=true \
+    persist.vendor.audio.avs.afe_api_version=2 \
+    persist.vendor.audio.fluence.audiorec=false \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.tmic.enabled=false \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.hifi.int_codec=true \
+    persist.vendor.audio.ras.enabled=false \
+    persist.vendor.audio.spv3.enable=true \
+    ro.af.client_heap_size_kbyte=7168 \
+    ro.vendor.audio.sdk.fluencetype=none \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.apptype.multirec.enabled=false \
     vendor.audio.dolby.ds2.enabled=false \
     vendor.audio.dolby.ds2.hardbypass=false \
-    vendor.audio.offload.multiple.enabled=false \
-    vendor.audio.offload.passthrough=false \
-    ro.vendor.audio.sdk.ssr=false \
-    vendor.audio.offload.gapless.enabled=true \
-    vendor.audio.safx.pbe.enabled=true \
-    vendor.audio.parser.ip.buffer.size=262144 \
     vendor.audio.flac.sw.decoder.24bit=true \
-    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
-    vendor.audio.use.sw.alac.decoder=true \
-    vendor.audio.use.sw.ape.decoder=true \
     vendor.audio.hw.aac.encoder=true \
     vendor.audio.noisy.broadcast.delay=600 \
-    persist.vendor.audio.hifi.int_codec=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
     vendor.audio.offload.pstimeout.secs=3 \
-    ro.af.client_heap_size_kbyte=7168 \
-    vendor.display.disable_ext_anim=1 \
-    qemu.hw.mainkeys=0 \
-    ro.opengles.version=196610 \
-    vendor.qcom.bluetooth.soc=cherokee \
-    ro.em.version=20 \
-    ro.vendor.qti.config.zram=true \
-    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
-    ro.vendor.iocgrp.config=1 \
-    debug.vendor.qti.enable.lm=1 \
-    sys.autosuspend.timeout=500000 \
-    ro.vendor.at_library=libqti-at.so \
-    persist.vendor.qti.games.gt.prof=1 \
-    ro.build.scafe.version=2019A \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.record.multiple.enabled=false \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio_hal.period_size=192 \
+    vendor.voice.path.for.pcm.voip=true
+
+# Bluetooth
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.bt.a2dp_offload_cap=sbc-aptx-aptxhd-aac-ldac \
+    vendor.qcom.bluetooth.soc=cherokee
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heaptargetutilization=0.75
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=360 \
-    ro.config.ringtone=Over_the_Horizon.ogg \
-    ro.config.notification_sound=Spaceline.ogg \
-    ro.config.alarm_alert=Homecoming.ogg \
-    ro.config.media_sound=Media_preview_Touch_the_light.ogg \
-    ro.config.ringtone_2=Atomic_Bell.ogg \
-    ro.config.notification_sound_2=Signal.ogg \
-    ro.hardware.keystore=mdfpp \
-    ro.hardware.gatekeeper=mdfpp \
-    ro.security.keystore.keytype=sak,gak \
-    ro.security.vpnpp.ver=2.1 \
-    ro.security.vpnpp.release=2.0 \
-    ro.config.dha_cached_max=12 \
-    ro.cfg.dha_2ndprop_thMB=4096 \
-    ro.cfg.dha_cached_max=20 \
-    ro.config.fha_enable=true \
-    ro.cfg.enable_userspace_lmk=true \
-    ro.cfg.kill_heaviest_task=true \
-    ro.config.freelimit_val=18 \
-    ro.config.custom_sw_limit=275 \
-    ro.config.upgrade_pressure=65 \
-    ro.cfg.custom_tm_limit=1000 \
-    ro.config.enable_upgrade_criadj=true \
-    ro.cfg.enable_upgrade_criadj=true \
-    ro.cfg.enable_reentry_lmk=true \
-    vendor.perf.iop_v3.enable=true \
+    ro.vendor.display.cabl=2
+
+# Gatekeeper
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.gatekeeper=mdfpp
+
+# GPS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.backup.ntpServer=0.pool.ntp.org
+
+# Graphics
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.egl.hw=0 \
+    debug.sf.hw=0 \
+    debug.sf.latch_unsignaled=1 \
+    persist.demo.hdmirotationlock=false \
+    ro.opengles.version=196610 \
+    vendor.display.disable_ext_anim=1 \
+    vendor.display.enable_default_color_mode=1
+
+# Keystore
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.keystore=mdfpp
 
 # OEM Unlock reporting
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.oem_unlock_supported=1
+
+# Perf
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.vendor.qti.enable.lm=1 \
+    persist.vendor.qti.games.gt.prof=1 \
+    ro.vendor.at_library=libqti-at.so \
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.iocgrp.config=1 \
+    ro.vendor.qti.config.zram=true \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60 \
+    vendor.perf.iop_v3.enable=true
+
+# Qualcomm System Daemon
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.qcomsysd.enabled=1
+
+# Radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.atfwd.start=false \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.vendor.radio.sib16_support=1
