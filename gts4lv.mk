71523f0de8086f4eef1b22dd2e6089d2404111b4
LuK1337 <priv.luk@gmail.com>
2019-08-14 23:08:39 +0200

    gts4lv-common: overlay: Set proper fingerprint location
    Change-Id: I8979cabe7cf36c164303209dc34f5c092d174bcc

--- gts4lv.mk
+++ gts4lv.mk
@@ -31,7 +31,8 @@ TARGET_SCREEN_WIDTH := 1600
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += \
     framework-res
