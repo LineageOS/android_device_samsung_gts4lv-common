40e05ce616e00c95c27e151261bf7e1d25fd7af9
LuK1337 <priv.luk@gmail.com>
2019-08-16 01:09:08 +0200

    gts4lv-common: Drop more blobs
    Change-Id: I05a3966c92a1101587a21f0820e2819870f72e17

--- gts4lv.mk
+++ gts4lv.mk
@@ -197,6 +197,7 @@ PRODUCT_COPY_FILES += \
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
+    libgui_vendor \
     libOmxAacEnc \
     libOmxAmrEnc \
     libOmxCore \
@@ -263,7 +264,8 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 # Thermal
 PRODUCT_PACKAGES += \
@@ -280,7 +282,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
     hostapd \
-    hostapd_cli \
     libqsap_sdk \
     libQWiFiSoftApCfg \
     libwifi-hal-qcom \
