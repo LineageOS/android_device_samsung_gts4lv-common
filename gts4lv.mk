9929aff8533bec24b5c94b494e5097ed848e5ec7
LuK1337 <priv.luk@gmail.com>
2019-08-16 14:19:33 +0200

    gts4lv-common: Blob libsensorndkbridge.so again
    * Camera doesn't work with OSS one ;_;
    
    Change-Id: I1e32f332bb219531fe81233ab199b8aef2c586e3

--- gts4lv.mk
+++ gts4lv.mk
@@ -268,8 +268,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
-    libsensorndkbridge
+    android.hardware.sensors@1.0-service
 
 # Thermal
 PRODUCT_PACKAGES += \
