6adea5a7eb3c3028d66af87bc4759187ed22eeb7
Bruno Martins <bgcngm@gmail.com>
2019-08-19 23:51:14 +0100

    gts4lv-common: Replace prebuilt power HAL with source built
    Change-Id: Ib3374f176929e514ec02bbd3c61ce33efce986d5

--- gts4lv.mk
+++ gts4lv.mk
@@ -257,7 +257,8 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service
+    android.hardware.power@1.0-service \
+    power.qcom
 
 # QCOM
 PRODUCT_PACKAGES += \
