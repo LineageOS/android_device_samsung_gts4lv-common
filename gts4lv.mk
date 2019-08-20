b491adbadfae9e374d943e11412213a30f1d9137
LuK1337 <priv.luk@gmail.com>
2019-08-20 03:53:07 +0200

    gts4lv-common: Build thermal HAL from source
    Change-Id: I9e646bc2ec578d2c4070be7dceee4cddf86febbf

--- gts4lv.mk
+++ gts4lv.mk
@@ -286,7 +286,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.sdm710
 
 # Wi-Fi
 PRODUCT_COPY_FILES += \
