add8f0c5c69a7c53b47e4343b833f012ab745637
Rashed Abdel-Tawab <rashed@linux.com>
2019-08-21 09:50:15 -0700

    gts4lv-common: Build device specific audio impl and remove 2.0 impls
    Change-Id: I99a09d22022e892a388bb8e6a5c45698603dd357

--- gts4lv.mk
+++ gts4lv.mk
@@ -52,11 +52,9 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio.effect@2.0-impl \
     android.hardware.audio.effect@4.0-impl \
-    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@4.0-impl.gts4lv \
     android.hardware.soundtrigger@2.0-impl:32 \
     android.hardware.soundtrigger@2.1-impl \
     audio.primary.sdm710:32 \
