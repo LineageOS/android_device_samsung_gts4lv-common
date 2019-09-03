6d3cb30721a7f0654dbe97821f605a5973254887
LuK1337 <priv.luk@gmail.com>
2019-09-03 13:25:21 +0200

    gts4lv-common: Build Trust HAL
    Change-Id: I9487a4903e1b2f896277ae87b6f85412abd9f8ed

--- gts4lv.mk
+++ gts4lv.mk
@@ -256,6 +256,10 @@ PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-service \
     thermal.sdm710
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.gts4lv
