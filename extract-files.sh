31be3ca33489040b969ce1a46f603c26aa2ceeba
LuK1337 <priv.luk@gmail.com>
2019-09-23 12:22:18 +0200

    gts4lv-common: Patch prebuilt P blobs to load libprocessgroup
    Change-Id: I0151324ec561e2c8c1bcd12fb87254de0d92706a

--- extract-files.sh
+++ extract-files.sh
@@ -30,6 +30,20 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function blob_fixup() {
+    case "${1}" in
+    vendor/lib/hw/audio.primary.sdm710.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+    vendor/lib/libgps.utils.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+    vendor/lib64/libgps.utils.so)
+        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
+        ;;
+    esac
+}
+
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
