0b50e40964e523f4b6f855b0b70ccbacd97452fc
Dan Pasanen <dan.pasanen@gmail.com>
2019-11-01 07:19:07 -0500

    gts4lv-common: Exclude lineage-sdk overlays from RRO
    * lineage-sdk overlays don't work with RRO
    
    Change-Id: Iff826faf1864388066a488acc9682636b98d1098

--- gts4lv.mk
+++ gts4lv.mk
@@ -39,6 +39,8 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
