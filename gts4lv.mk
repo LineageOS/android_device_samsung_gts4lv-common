e803c2253aa7e2bbf6c3ecfd1d2592f9b34cf502
Bruno Martins <bgcngm@gmail.com>
2020-03-07 15:31:21 +0000

    gts4lv-common: Split system and vendor props
    Change-Id: I6d5cdf4b4b1671a9631e81f3242c9abccf0ffe2d

--- gts4lv.mk
+++ gts4lv.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2019-2020 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -40,9 +40,6 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += \
     framework-res
-
-# Properties
--include $(LOCAL_PATH)/vendor_prop.mk
 
 # Additional native libraries
 PRODUCT_COPY_FILES += \
