85a4bfc2bcebd191d087925fb52770f9422e842a
Jack Pham <jackp@codeaurora.org>
2020-03-12 23:59:56 -0700

    gts4lv-common: init.qcom.usb.rc: Consolidate init.msm.usb.configfs.rc
    Remove obsolete android_usb based property triggers now
    that configfs is always used. Merge in init.msm.configfs.rc
    so we now have just a single .rc init file.
    
    Also remove all other references to /sys/class/android_usb/*.
    
    Change-Id: I06eba8e715ccee5974dc9de8ac19df6c79ae0c45

--- gts4lv.mk
+++ gts4lv.mk
@@ -105,7 +105,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.class_main.sh \
-    init.msm.usb.configfs.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.power.rc \
