6b76fce4d3aa346936ecc414611a1b8cd1b62ec9
Bruno Martins <bgcngm@gmail.com>
2020-06-27 11:03:00 +0100

    gts4lv-common: Update all the things to Q
    Change-Id: Ia53db2e488fe96d41083da686a42811da46d291a

--- extract-files.sh
+++ extract-files.sh
@@ -30,23 +30,6 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
-function blob_fixup() {
-    case "${1}" in
-    vendor/lib/hw/vulkan.sdm710.so)
-        sed -i "s/vulkan.sdm845.so/vulkan.sdm710.so/g" "${2}"
-        ;;
-    vendor/lib/libgps.utils.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    vendor/lib64/hw/vulkan.sdm710.so)
-        sed -i "s/vulkan.sdm845.so/vulkan.sdm710.so/g" "${2}"
-        ;;
-    vendor/lib64/libgps.utils.so)
-        patchelf --replace-needed "libcutils.so" "libprocessgroup.so" "${2}"
-        ;;
-    esac
-}
-
 # Default to sanitizing the vendor folder before extraction
 CLEAN_VENDOR=true
 
