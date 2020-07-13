f9da06e1c3ab2b39fd938b344dc1ef23f6709e70
LuK1337 <priv.luk@gmail.com>
2020-07-13 23:28:00 +0200

    gts4lv-common: Switch to common qcom power HAL
    Change-Id: I59d039c500c672c50727aa715c256c9f55d98fa4

--- power/Android.mk
+++ power/Android.mk
@@ -2,45 +2,13 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := android.hardware.power@1.2-service.samsung
-LOCAL_INIT_RC := android.hardware.power@1.2-service.samsung.rc
-LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE := libpower_feature.gts4lv
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_VENDOR_MODULE := true
 
-LOCAL_SRC_FILES := \
-    hint-data.c \
-    list.c \
-    metadata-parser.c \
-    power-710.c \
-    power-common.c \
-    powerhintparser.c \
-    Power.cpp \
-    service.cpp \
-    utils.c
+LOCAL_SRC_FILES := power-feature.c
 
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
+LOCAL_HEADER_LIBRARIES := libhardware_headers qti_powerhal_headers
 
-LOCAL_SHARED_LIBRARIES := \
-    android.hardware.power@1.2 \
-    libbase \
-    libcutils \
-    libdl \
-    libhidlbase \
-    libhidltransport \
-    liblog \
-    libutils \
-    libxml2
-
-LOCAL_HEADER_LIBRARIES += libutils_headers
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-
-ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
-    LOCAL_CFLAGS += -DINTERACTION_BOOST
-endif
-
-LOCAL_CFLAGS += -Werror -Wall -Wno-unused-parameter
-
-include $(BUILD_EXECUTABLE)
+include $(BUILD_STATIC_LIBRARY)
