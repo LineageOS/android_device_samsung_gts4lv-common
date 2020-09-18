e4e4f5662e22e88c0bdb6923848d1cb7278220a5
LuK1337 <priv.luk@gmail.com>
2020-09-18 12:49:21 +0200

    [DNM] gts4lv-common: Common bring up changes
    Change-Id: Ic4c087a3a5da28115ee266cf28c69a93cc22f4ca

--- gts4lv.mk
+++ gts4lv.mk
@@ -324,8 +324,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libnl
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-wfd.xml:system/etc/permissions/privapp-permissions-wfd.xml
