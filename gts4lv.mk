cf4ee214c4a5746f90cbd3fbd75966c32abd5681
LuK1337 <priv.luk@gmail.com>
2020-09-18 13:12:09 +0200

    gts4lv-common: Drop android.hardware.thermal HAL
    android.hardware.thermal@1.0::IThermal/default is deprecated in compatibility matrix at FCM Version 4; it should not be served.
    
    Change-Id: Icc3c672a0bf6b9e4ee76ccf91e6cdca5fb778fde

--- gts4lv.mk
+++ gts4lv.mk
@@ -284,12 +284,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.sdm710
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
