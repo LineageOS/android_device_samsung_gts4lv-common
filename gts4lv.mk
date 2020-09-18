50c15e956c2e84ba3d31641c024fbf491ec9b7a3
LuK1337 <priv.luk@gmail.com>
2020-09-18 14:36:05 +0200

    gts4lv-common: Build libhidltransport/libhwbinder
    * Needed for old blobs
    
    Change-Id: I43fc0e46547dfc126a4b2ac9dcc82c7c4f2ee172

--- gts4lv.mk
+++ gts4lv.mk
@@ -168,7 +168,9 @@ PRODUCT_PACKAGES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor
+    android.hidl.base@1.0.vendor \
+    libhidltransport \
+    libhwbinder
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
