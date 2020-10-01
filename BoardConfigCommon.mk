5bef6f4c5ca9fc97ab8c59c9a7a266b7d83f2b3a
LuK1337 <priv.luk@gmail.com>
2020-10-01 14:15:07 +0200

    gts4lv-common: Switch to TARGET_POWERHAL_MODE_EXT
    Change-Id: I9a221cbac850a26e751aa6a6e2e6ae00bacc8cf5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,7 +192,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_USES_MKE2FS := true
 
 # Power
-TARGET_POWER_SET_FEATURE_LIB := libpower_feature.gts4lv
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(COMMON_PATH)/odm.prop
