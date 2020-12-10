a6a4c9a1738893aaa58003f06f782cddf1cfcf0f
Bruno Martins <bgcngm@gmail.com>
2020-12-10 14:51:56 +0000

    gts4lv-common: Fixup Qualcomm DPM blobs
    Not only libdpmframework depends on the already known to be missing
    `strdup8to16` symbol, it is also nicer to move the packages from
    /product to /system_ext rather than adding stupid sepolicies for dpmd.
    
    Change-Id: I6e649d518f554071df09b7a578f2220565391b16

--- gts4lv.mk
+++ gts4lv.mk
@@ -302,6 +302,7 @@ PRODUCT_PACKAGES += \
 
 # VNDK
 PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libcutils-v29.so \
     prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcutils-v29.so
 
 # WiFi
