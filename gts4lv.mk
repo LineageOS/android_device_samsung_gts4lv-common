ebb01ddf2fa518677b6b35776b5daf4f47411c53
Bruno Martins <bgcngm@gmail.com>
2021-04-10 16:47:04 +0100

    gts4lv-common: Drop all(most) pre-R blob hax
    Sadly /system/lib64/libantradio.so still depends on libhidltransport
    and libhwbinder.
    
    Change-Id: Ie70ba40d6ae4f5344a44be6565aa6bc1cca66a35

--- gts4lv.mk
+++ gts4lv.mk
@@ -167,12 +167,8 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
     libhidltransport \
-    libhidltransport.vendor \
-    libhwbinder \
-    libhwbinder.vendor
+    libhwbinder
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
@@ -256,11 +252,6 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.0.vendor
 
-# Protobuf
-PRODUCT_PACKAGES += \
-    libprotobuf-cpp-full-vendorcompat \
-    libprotobuf-cpp-lite-vendorcompat
-
 # QCOM
 PRODUCT_PACKAGES += \
     libjson \
@@ -298,10 +289,6 @@ PRODUCT_PACKAGES += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.1-service.gts4lv
-
-# VNDK
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-sp/libcutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libcutils-v29.so
 
 # WiFi
 PRODUCT_COPY_FILES += \
