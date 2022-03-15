551f04b86c9f04326f66132a9664ac5da95a9802
Bruno Martins <bgcngm@gmail.com>
2022-03-15 14:19:48 +0000

    gts4lv-common: Update blobs from T720XXU2DUL1
    Change-Id: I37228dddda84aa0e95c24bdc5a5940daff5ab7e1

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2021 The LineageOS Project
+# Copyright (C) 2019-2022 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -164,7 +164,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 BOARD_ROOT_EXTRA_FOLDERS := efs
 
 # Security Patch Level
-VENDOR_SECURITY_PATCH := 2021-08-01
+VENDOR_SECURITY_PATCH := 2021-12-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
