02cee4ab2ac234e329ecd6e0c73b1dae23b55876
Bruno Martins <bgcngm@gmail.com>
2022-03-15 15:42:49 +0000

    gts4lv-common: Drop device/samsung/qcom-common dependency
    There's nothing useful in there anymore, now that
    config_restartActionsList is overlayed here.
    
    Change-Id: Id3d20d8ee574fb598b7c685c6adba6582a6f2008

--- gts4lv.mk
+++ gts4lv.mk
@@ -35,8 +35,7 @@ TARGET_SCREEN_WIDTH := 1600
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage \
-    device/samsung/qcom-common/overlay
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
