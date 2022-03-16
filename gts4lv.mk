83c00109d162a594bc6d8253ce65ceacf3c46e16
LuK1337 <priv.luk@gmail.com>
2022-03-16 01:30:14 +0100

    gts4lv-common: Remove trust HAL from product packages
    USB 1.3 HAL now provides the same functionality and even more.
    
    Change-Id: Idda908a90d3b935e1065eb41abec4babeefc4fd0

--- gts4lv.mk
+++ gts4lv.mk
@@ -309,10 +309,6 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0.vendor
 
-# Trust HAL
-PRODUCT_PACKAGES += \
-    vendor.lineage.trust@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
