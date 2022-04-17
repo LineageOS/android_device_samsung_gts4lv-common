a792548f516c34fee70cbc27ac7b777f029eebfa
Tim Zimmermann <tim@linux4.de>
2022-04-17 16:40:02 +0200

    gts4lv-common: Set SEC specific BT SCO sample rate audio param
    This lets us convert AOSP's bt_wbs to SEC g_sco_samplerate.
    
    Change-Id: I78d5d36044421daf99b8a13aa417060a3cf959a3

--- audio/impl/ParametersUtil.cpp
+++ audio/impl/ParametersUtil.cpp
@@ -182,7 +182,10 @@ Result ParametersUtil::setParametersImpl(const hidl_vec<ParameterValue>& context
         params.add(String8(pair.key.c_str()), String8(pair.value.c_str()));
     }
     for (size_t i = 0; i < parameters.size(); ++i) {
-        if (parameters[i].key == "rotation") {
+        if (parameters[i].key == "bt_wbs") {
+            params.add(String8("g_sco_samplerate"),
+                       String8(parameters[i].value == AudioParameter::valueOn ? "16000" : "8000"));
+        } else if (parameters[i].key == "rotation") {
             if (parameters[i].value == "0") {
                 setSlotPositions(SLOT_POSITIONS_0);
             } else if (parameters[i].value == "90") {
