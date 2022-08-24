9fa92c4384e5ff76f2eca732068df2278e3f924d
Tim Zimmermann <tim@linux4.de>
2022-08-24 15:40:22 +0200

    gts4lv-common: Inherit non_ab_device.mk
    * This is required to build applypatch for updating recovery
    
    Change-Id: I38053691f7724b5d6c30159c410d5d04ac136180

--- gts4lv.mk
+++ gts4lv.mk
@@ -14,6 +14,7 @@
 # limitations under the License.
 #
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Enable updating of APEXes
