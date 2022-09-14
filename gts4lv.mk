b7a0e557c17a8499949837fe4b61a12cee703567
Bruno Martins <bgcngm@gmail.com>
2022-09-14 14:51:15 +0100

    gts4lv-common: Migrate to AIDL ClearKey DRM HAL
    Change-Id: Iad5cc1176978137c647069197b33f49fb4d98ab2

--- gts4lv.mk
+++ gts4lv.mk
@@ -150,7 +150,7 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 # fastbootd
 PRODUCT_PACKAGES += \
