f167eea3020defe99d6f7e48507252b2c33f5285
LuK1337 <priv.luk@gmail.com>
2023-06-19 14:36:39 +0200

    gts4lv-common: Set WiFi country code based on CSC
    Fixes: https://gitlab.com/LineageOS/issues/android/-/issues/5973
    Change-Id: I81b161d5acadc3bc12a04bbdbbe63fc5a43d71eb

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -95,7 +95,7 @@ BOARD_BOOT_HEADER_VERSION := 1
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=1 loop.max_part=7
-BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0 androidboot.wificountrycode=00
+BOARD_KERNEL_CMDLINE += androidboot.vbmeta.avb_version=1.0
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_OFFSET := 0x00008000
 BOARD_KERNEL_PAGESIZE := 4096
