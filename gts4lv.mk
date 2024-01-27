e6faf9c2b37666902524a97820fecb20fe085d6f
LuK1337 <priv.luk@gmail.com>
2024-01-27 15:21:27 +0100

    gts4lv-common: Switch to AIDL fingerprint HAL
    Change-Id: I588989bdc0ad8e21f0068664213899c10cb5b81d

--- gts4lv.mk
+++ gts4lv.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2023 The LineageOS Project
+# Copyright (C) 2019-2024 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -163,7 +163,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.samsung
+    android.hardware.biometrics.fingerprint-service.samsung
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
