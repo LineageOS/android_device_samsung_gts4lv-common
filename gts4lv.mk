1f9948ce821c0bd3bcfd865f999c97f2c1d0444f
basamaryan <basam.aryan@gmail.com>
2024-03-20 01:16:12 -0400

    gts4lv-common: Don't explicitly include updatable_apex.mk
    It is inherited by base_system.mk as of QPR2
    
    Change-Id: I020a05b233f61ea28d03199cbc76051e6e97e1dc

--- gts4lv.mk
+++ gts4lv.mk
@@ -17,9 +17,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/samsung/gts4lv-common/gts4lv-common-vendor.mk)
 
