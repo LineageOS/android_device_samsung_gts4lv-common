86a0e384da8277a52fce2efb4f056d4d7c8801f0
Michael Bestas <mikeioannina@gmail.com>
2024-06-14 00:26:49 +0300

    gts4lv-common: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2

--- gts4lv.mk
+++ gts4lv.mk
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.cameraservice.device@2.0.vendor \
     android.frameworks.cameraservice.service@2.1.vendor \
     android.hardware.camera.provider@2.5-service.samsung \
+    libcamera_metadata.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 # Charger
@@ -188,6 +189,7 @@ PRODUCT_PACKAGES += \
     android.hidl.allocator@1.0.vendor \
     android.hidl.base@1.0 \
     android.hidl.memory@1.0.vendor \
+    libhidlmemory.vendor:64 \
     libhidltransport \
     libhwbinder \
     libhwbinder.vendor
@@ -285,8 +287,10 @@ PRODUCT_PACKAGES += \
 # QCOM
 PRODUCT_PACKAGES += \
     libjson \
+    libjsoncpp.vendor:64 \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
+    libsqlite.vendor:64 \
     libtinyxml
 
 # QTI
@@ -350,6 +354,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.media.audio.common.types-V2-cpp \
     libnl \
+    libion.vendor \
+    libpng.vendor:32 \
     libwfdaac_vendor
 
 PRODUCT_BOOT_JARS += \
