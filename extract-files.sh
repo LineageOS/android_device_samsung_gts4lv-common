63ccb0f0e91b72a61d641720e36e9bc23f360eb8
Bruno Martins <bgcngm@gmail.com>
2024-06-30 15:43:03 +0100

    gts4lv-common: Switch to health AIDL HAL
    Change-Id: I0600fbe80dbd538dfe2aef0538b68e4e399c915e

--- extract-files.sh
+++ extract-files.sh
@@ -75,10 +75,6 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --add-needed "libshim_sensorndkbridge.so" "${2}"
             ;;
-        vendor/lib64/hw/android.hardware.health@2.0-impl-2.1-samsung.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v30.so" "${2}"
-            ;;
         vendor/lib64/hw/gatekeeper.mdfpp.so|vendor/lib64/libskeymaster4device.so)
             [ "$2" = "" ] && return 0
             "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "${2}"
