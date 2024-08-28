5eaf182b74304e5a3297b41fe2ae748ba45bede2
LuK1337 <priv.luk@gmail.com>
2024-08-28 15:32:59 +0200

    gts4lv-common: Migrate to soong_config_set()
    Change-Id: I06de8759836114f54a822d6f23f1177515e21178

--- gts4lv.mk
+++ gts4lv.mk
@@ -86,6 +86,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
 # Camera
+$(call soong_config_set,samsungCameraVars,needs_sec_reserved_field,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider-service_32.samsung
 
