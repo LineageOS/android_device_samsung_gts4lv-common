101018563b689036f4950b6147e645edc632884b
Bruno Martins <bgcngm@gmail.com>
2024-09-28 16:30:33 +0100

    gts4lv-common: Setup ELF checks
    Change-Id: I09130799d80e7abb8b694f5b8fe7266a5ea74fb4

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -22,6 +22,43 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"device/samsung/gts4lv-common",
+		"hardware/qcom-caf/sdm845",
+		"hardware/qcom-caf/wlan",
+		"vendor/qcom/opensource/dataservices",
+        "vendor/qcom/opensource/commonsys/display",
+        "vendor/qcom/opensource/commonsys-intf/display",
+        "vendor/qcom/opensource/display",
+EOF
+}
+
+function lib_to_package_fixup_vendor_variants() {
+    if [ "$2" != "vendor" ]; then
+        return 1
+    fi
+
+    case "$1" in
+        com.qualcomm.qti.ant@1.0 | \
+            libmmosal | \
+            vendor.qti.hardware.wifidisplaysession@1.0)
+            echo "$1_vendor"
+            ;;
+        libOmxCore | \
+            libwpa_client) ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+function lib_to_package_fixup() {
+    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
+        lib_to_package_fixup_proto_3_9_1 "$1" ||
+        lib_to_package_fixup_vendor_variants "$@"
+}
+
 # Initialize the helper for common
 setup_vendor "${DEVICE_COMMON}" "${VENDOR}" "${ANDROID_ROOT}" true
 
