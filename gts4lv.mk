182f67aa269b3f1279f83812b9ebacdc4406997e
Bruno Martins <bgcngm@gmail.com>
2024-09-28 18:03:37 +0100

    gts4lv-common: Remove old VNDK prebuilt lib copy
    Change-Id: I96f7c9fdf143bab40f2143acad16316ad187415b

--- gts4lv.mk
+++ gts4lv.mk
@@ -333,9 +333,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcrypto-v33
 
-PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-sp/libutils.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libutils-v30.so
-
 # WiFi
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/indoorchannel.info:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/indoorchannel.info \
