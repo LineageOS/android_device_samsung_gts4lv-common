0e19a5a676a0989ad81fcaa438c9ee0972560f8d
Christian Hoffmann <chrmhoffmann@gmail.com>
2024-09-29 18:39:07 +0200

    gts4lv-common: Align with new recovery structure
    Change-Id: Ic3ecf63a2888aac08aeaf40677ae7bfa74912030

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_C_INCLUDES := \
     bootable/recovery \
-    bootable/recovery/edify/include \
+    bootable/deprecated-ota/edify/include \
     bootable/recovery/otautil/include \
-    bootable/recovery/updater/include \
+    bootable/deprecated-ota/updater/include \
     system/libbase/include
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_samsung
