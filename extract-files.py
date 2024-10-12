6ef33d08bfbe47413261355d17b8d3afdd38e9f3
LuK1337 <priv.luk@gmail.com>
2024-10-12 17:42:02 +0200

    gts4lv-common: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5

--- extract-files.py
+++ extract-files.py
@@ -51,8 +51,12 @@ lib_fixups: lib_fixups_user_type = {
 }
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib/libwfdmmsrc_system.so': blob_fixup()
+        .add_needed('libgui_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V3-cpp.so'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
     ('vendor/lib/mediadrm/libwvdrmengine.so', 'vendor/lib/libwvhidl.so'): blob_fixup()
         .add_needed('libcrypto_shim.so'),
     'vendor/lib/libsensorlistener.so': blob_fixup()
