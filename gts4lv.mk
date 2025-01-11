bd02918a3975bb7a06b920e9da078f67236f4f27
Bruno Martins <bgcngm@gmail.com>
2025-01-11 17:43:33 +0000

    gts4lv-common: Rename rootdir to init and migrate to blueprints
    Change-Id: I6db1f9423bb352cc8b0f3559ecbb5c7ba60e26ac

--- gts4lv.mk
+++ gts4lv.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2019-2024 The LineageOS Project
+# Copyright (C) 2019-2025 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -96,6 +96,7 @@ PRODUCT_PACKAGES += \
 # Common init scripts
 PRODUCT_PACKAGES += \
     fstab.qcom \
+    fstab.qcom.ramdisk \
     init.class_main.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
