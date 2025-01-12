aca119e18e8eda493bdb7a991d5b422dd89ae7f2
Bruno Martins <bgcngm@gmail.com>
2025-01-12 09:05:07 +0000

    gts4lv-common: Drop all Android makefiles inclusion within the tree
    Change-Id: I33a163d7243fe1203538d6e37a907acd3976f1d4

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2024 The LineageOS Project
+# Copyright (C) 2018-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -7,8 +7,6 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter gts4lv gts4lvwifi,$(TARGET_DEVICE)),)
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
 
 DSP_SYMLINK := $(TARGET_OUT_VENDOR)/lib/dsp
 $(DSP_SYMLINK): $(LOCAL_INSTALLED_MODULE)
