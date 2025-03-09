8b8edd3fc36c63c9190b4e1b90d574db5f95a6c3
Bruno Martins <bgcngm@gmail.com>
2025-03-09 16:14:43 +0000

    gts4lv-common: Drop TARGET_KEYMASTER_VARIANT flag
    Samsung keymaster HAL moved to blueprint.
    
    Change-Id: I992a12f1ddc5769ddb9fb232ea2a287c1d654da6

--- gts4lv.mk
+++ gts4lv.mk
@@ -183,6 +183,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0-service.samsung
 
+$(call soong_config_set,samsung_keymaster,SKEYMASTER4DEVICE_LIBRARY,"//vendor/samsung/gts4lv-common:libskeymaster4device")
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
